fa27af0dc6b98105b43dddce03ff73ec80c86199
Gopal Khatri <khatri.gopal@gmail.com>
2023-09-16 20:55:03 +0530

    added lambda value:

--- function1/function1.py
+++ function1/function1.py
@@ -1,4 +1,3 @@
 def lambda_handler (event, context):
-   print ("Welcome to Second Lambda Fumction Pipeline")
-   print ("Welome to Persistent")
-   
+   print ("Welcome to my Fumction Pipeline")
+  
